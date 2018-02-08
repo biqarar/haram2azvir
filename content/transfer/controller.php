ae27d5702574f5301b1fd56005d02f5a0344869f
Reza Mohiti <rm.biqarar@gmail.com>
2018-02-08 10:59:18 +0330

    fix send plan file

--- content/transfer/controller.php
+++ content/transfer/controller.php
@@ -7,6 +7,9 @@ class controller extends \content\main\controller
 	// for routing check
 	function ready()
 	{
+		// $azvir = new \lib\utility\ermile\azvir('$2y$07$ppqEBLfiUT7zlqFVxp/Wa.ILdtNVkL/i1o2eAZ4Dm41mV9VlSiU86', 'haram', 1);
+		// $add_group = $azvir->pricetype('post', ['title' => 'حفظ', 'price' => 2000, 'unittype' => 'real']);
+		// var_dump($add_group);exit();
 
 		$this->post('transfer')->ALL();
 	}
--- content/transfer/model/planfile.php
+++ content/transfer/model/planfile.php
@@ -64,16 +64,170 @@ trait planfile
 		$course = array_unique($course);
 		$course = array_values($course);
 
-		$price  = array_column($rows, 'price');
-		$price  = array_filter($price);
-		$price  = array_unique($price);
-		$price  = array_values($price);
+		$pricetype   = array_column($rows, 'price');
+		$pricetype   = array_filter($pricetype);
+		$pricetype   = array_unique($pricetype);
+		$pricetype   = array_values($pricetype);
+		$pricetype[] = '0';
 
+		$azvir = new \lib\utility\ermile\azvir('$2y$07$ppqEBLfiUT7zlqFVxp/Wa.ILdtNVkL/i1o2eAZ4Dm41mV9VlSiU86', 'haram', 1);
+		$azvir_group = [];
+		foreach ($group as $key => $value)
+		{
+			$group_id = self::fix($azvir->group('post', ['title' => $value]));
+			if(isset($group_id['id']))
+			{
+				$azvir_group[$group_id['id']] = $value;
+			}
+			else
+			{
+				$group_id = self::fix($azvir->group_search('get', ['search' => $value]));
+				if(isset($group_id[0]['id']))
+				{
+					$azvir_group[$group_id[0]['id']] = $value;
+				}
+				else
+				{
+					\lib\debug::warn("Can not add group $value");
+				}
+			}
+		}
 
-		var_dump($group, $grade, $course, $price);
+		$azvir_grade = [];
+		foreach ($grade as $key => $value)
+		{
+			$grade_id = self::fix($azvir->grade('post', ['title' => $value]));
+			if(isset($grade_id['id']))
+			{
+				$azvir_grade[$grade_id['id']] = $value;
+			}
+			else
+			{
+				$grade_id = self::fix($azvir->grade_search('get', ['search' => $value]));
+				if(isset($grade_id[0]['id']))
+				{
+					$azvir_grade[$grade_id[0]['id']] = $value;
+				}
+				else
+				{
+					\lib\debug::warn("Can not add grade $value");
+				}
+			}
+		}
+
+		$azvir_course = [];
+		foreach ($course as $key => $value)
+		{
+			$course_id = self::fix($azvir->course('post', ['title' => $value]));
+			if(isset($course_id['id']))
+			{
+				$azvir_course[$course_id['id']] = $value;
+			}
+			else
+			{
+				$course_id = self::fix($azvir->course_search('get', ['search' => $value]));
+				if(isset($course_id[0]['id']))
+				{
+					$azvir_course[$course_id[0]['id']] = $value;
+				}
+				else
+				{
+					\lib\debug::warn("Can not add course $value");
+				}
+			}
+		}
+
+		$azvir_pricetype = [];
+		foreach ($pricetype as $key => $value)
+		{
+			$pricetype_id = self::fix($azvir->pricetype('post', ['title' => $value, 'price' => $value, 'unittype' => 'real']));
+			if(isset($pricetype_id['id']))
+			{
+				$azvir_pricetype[$pricetype_id['id']] = $value;
+			}
+			else
+			{
+				$pricetype_id = self::fix($azvir->pricetype_search('get', ['price' => $value]));
+
+				if(isset($pricetype_id[0]['id']))
+				{
+					$azvir_pricetype[$pricetype_id[0]['id']] = $value;
+
+				}
+				else
+				{
+					\lib\debug::warn("Can not add pricetype $value");
+				}
+			}
+		}
+
+		$azvir_topic = [];
+		foreach ($rows as $key => $value)
+		{
+			$insert_topic =
+			[
+
+				'grade_id'      => array_search($value['grade'], $azvir_grade),
+				'group_id'      => array_search($value['group'], $azvir_group),
+				'course_id'     => array_search($value['course'], $azvir_course),
+				'pricetype_id'  => array_search($value['price'], $azvir_pricetype),
+				'title'         => $value['title'],
+				'totaltime'     => $value['otime'] == 'NULL' ? null : $value['otime'],
+				'passscore'     => $value['omark'],
+				'certification' => $value['osertification'] === 'no' ? false : true,
+				'learnunit'     => 1,
+			];
+
+			$topic_id = self::fix($azvir->topic('post', $insert_topic), true, [$insert_topic, $value]);
+			if(isset($topic_id['id']))
+			{
+				$azvir_topic[$topic_id['id']] = $value;
+			}
+			else
+			{
+				$search_topic =
+				[
+					'search'    => $value['title'],
+					'grade_id'  => array_search($value['grade'], $azvir_grade),
+					'group_id'  => array_search($value['group'], $azvir_group),
+					'course_id' => array_search($value['course'], $azvir_course),
+				];
+
+				$topic_id = self::fix($azvir->topic_search('get', $search_topic));
+				if(isset($topic_id[0]['id']))
+				{
+					$azvir_topic[$topic_id[0]['id']] = $value;
+				}
+				else
+				{
+					\lib\debug::warn('Can not add topic');
+				}
+			}
+		}
+		\lib\debug::true("حله. بریم بعدی");
+
+		// var_dump($group, $grade, $course, $price);
 		// var_dump($first_rows, $rows);
 		// var_dump($old_plan);
-		exit();
+		// exit();
+	}
+
+
+	public static function fix($_responce, $_var_dump = false, $_data = [])
+	{
+
+		$text = null;
+
+		$text .= json_encode($_responce, JSON_UNESCAPED_UNICODE). "\n";
+		$text .= json_encode($_data, JSON_UNESCAPED_UNICODE). "\n\n";
+
+		file_put_contents(__DIR__. '/log',$text , FILE_APPEND);
+
+		if(isset($_responce['result']) && $_responce['result'])
+		{
+			return $_responce['result'];
+		}
+		return null;
 	}
 
 }
