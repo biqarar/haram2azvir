51242faf06aa2f1f59ac94b9e39b0b00dae28add
Reza <rm.biqarar@gmail.com>
2018-06-01 09:29:41 +0430

    add trans

---
+++ includes/languages/trans_project.php
@@ -0,0 +1,16 @@
+<?php
+class twigTransTerms
+{
+ private function transtext()
+ {
+
+	//-------------------------------------------------------content/home/display.html
+
+	//--------------------------------------------includes/languages/trans_static.html
+	echo T_("Dash");                                                                  // Line 7 Seperate
+
+	//---------------------public_html/static/siftal/fonts/siftal/icons-reference.html
+
+ }
+}
+?>
