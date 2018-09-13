487b321a3fc610d97a60c2f8e073d9533fc1d7ef
Reza <rm.biqarar@gmail.com>
2018-09-14 00:18:59 +0430

    fix certification

--- content/transfer/model/certification.php
+++ content/transfer/model/certification.php
@@ -8,22 +8,47 @@ trait certification
 	{
 		$query = [];
 
-		$query[] = "ALTER TABLE `classification` ADD `azvir_topic_id` varchar(100) NULL DEFAULT NULL";
-		$query[] = "UPDATE `classification` SET `azvir_topic_id` = (SELECT azvir_topic_id from classes where classes.id = classification.classes_id LIMIT 1)";
+		// $query[] = "ALTER TABLE `classification` ADD `azvir_topic_id` varchar(100) NULL DEFAULT NULL";
+		// $query[] = "UPDATE `classification` SET `azvir_topic_id` = (SELECT azvir_topic_id from classes where classes.id = classification.classes_id LIMIT 1)";
+
+		// $query[] = "ALTER TABLE `classification` ADD `plan_id` varchar(100) NULL DEFAULT NULL";
+		// $query[] = "UPDATE `classification` SET `plan_id` = (SELECT plan_id from classes where classes.id = classification.classes_id LIMIT 1)";
+
+		// $query[] = "ALTER TABLE `certification` ADD `plan_id` varchar(100) NULL DEFAULT NULL";
+		// $query[] = "UPDATE `certification` SET `plan_id` = (SELECT plan_id from classification where classification.id = certification.classification_id LIMIT 1)";
 
 
-		$query[] = "ALTER TABLE `certification` ADD `azvir_topic_id` varchar(100) NULL DEFAULT NULL";
-		$query[] = "UPDATE `certification` SET `azvir_topic_id` = (SELECT azvir_topic_id from classification where classification.id = certification.classification_id LIMIT 1)";
+		// $query[] = "ALTER TABLE `certification` ADD `azvir_member_id` varchar(100) NULL DEFAULT NULL";
+		// $query[] = "UPDATE `certification` SET `azvir_member_id` = (SELECT azvir_member_id from classification where classification.id = certification.classification_id LIMIT 1)";
+
+		// $query[] = "ALTER TABLE `certification` ADD `plan_name` varchar(200) NULL DEFAULT NULL";
+		// $query[] = "UPDATE `certification` SET `plan_name` = (SELECT plan.name from plan where plan.id = certification.plan_id LIMIT 1)";
 
 
 
-		foreach ($query as $key => $value)
+		// foreach ($query as $key => $value)
+		// {
+		// 	\dash\db::query($value, 'quran_hadith');
+		// }
+
+		$azvir = new \dash\utility\ermile\azvir(azvir_api_key, azvir_api_school, 1);
+
+		$query = "SELECT azvir_member_id, GROUP_CONCAT(plan_name) AS `X` FROM certification group by azvir_member_id ";
+		$result = \dash\db::get($query, null, false, 'quran_hadith');
+		foreach ($result as $key => $value)
 		{
-			\dash\db::query($value, 'quran_hadith');
+			$patch =
+			[
+				'id'  => $value['azvir_member_id'],
+				'desc' => "گواهی‌نامه‌های ثبت شده در پورتال قدیم: ". $value['X'],
+			];
+
+			$xpatch = $azvir->member('patch', $patch);
 		}
-		// case 'certification': add new certification
-		$args['name']   = $name;
-		$args['status'] = 'enable';
+
+		// // case 'certification': add new certification
+		// $args['name']   = $name;
+		// $args['status'] = 'enable';
 
 		// case 'certification/detail': add certification topic id
 		// 		if(isset($input['topic_id']) && isset($input['certification_id']))
